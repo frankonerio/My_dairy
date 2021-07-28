e5e40fb669e96931184f4c2007a4f335aaa5228c
Franklin <frankonerio@gmail.com>
2021-07-28 08:18:44 +0200

    deploy to Heroku

--- API/server.js
+++ API/server.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import index from './Routes';
 
+const PORT = process.env.PORT || 8888;
 const server = express();
 function displayServerMessage() {
     return console.log('My Diary API server is listening on Port 8888');
 }
 server.use(index);
-const PORT = process.env.PORT || 8888;
 server.listen(PORT, displayServerMessage);
 
-export default server;
+export default server;  
