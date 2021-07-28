c4308a2e9a0bf0db98d2fe79c14329427a187f24
Franklin <frankonerio@gmail.com>
2021-07-28 13:06:40 +0200

    heroku

--- API/Routes/HomePageRouter.js
+++ API/Routes/HomePageRouter.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 const HomepageRouter = express.Router();
 
-HomepageRouter.use(express.static(path.join(__dirname, '..','..', '/UI/Landingpage.html')));
+HomepageRouter.use(express.static(path.join(__dirname, '../UI', 'Landingpage.html')));
 
 HomepageRouter.get('/', HomePageController.fetchHomePage);
 
