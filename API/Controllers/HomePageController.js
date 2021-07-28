e14677a3abfe98ebd7f7f3f8b8a35ca7710affcb
Franklin <frankonerio@gmail.com>
2021-07-28 13:58:02 +0200

    heroku

--- API/Controllers/HomePageController.js
+++ API/Controllers/HomePageController.js
@@ -5,9 +5,10 @@ class HomePageController {
 
     static fetchHomePage(request, response){
 
-        return response.sendFile(path.join(__dirname, '../../UI/landingpage.html'));
+        return response.sendFile(path.join(__dirname, '../../../UI/landingpage.html'))
     
     }
 }
 
 export default HomePageController;
+
